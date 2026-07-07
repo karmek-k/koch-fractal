f4ad97b5efcd29b5aa079ba0489a5d0337512e6f
karmek-k <33288445+karmek-k@users.noreply.github.com>
2026-07-07 16:56:09 +0200

    feat: fractal level slider

--- koch.js
+++ koch.js
@@ -1,10 +1,35 @@
 console.log("koch.js loaded");
 
 const canvas = document.getElementById("koch-canvas");
-
 if (!canvas) {
   throw new Exception("Canvas not found");
 }
 
+const slider = document.getElementById("slider");
+if (!slider) {
+  throw new Exception("Slider not found");
+}
+
+const w = canvas.getAttribute("width");
+const h = canvas.getAttribute("height");
 const ctx = canvas.getContext("2d");
-ctx.fillRect(0, 0, 100, 100);
+
+function drawLine(ctx, beginning, end) {
+  ctx.moveTo(beginning.x, beginning.y);
+  ctx.lineTo(end.x, end.y);
+  ctx.moveTo(0, 0);
+}
+
+function drawKochFractal(level) {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, w, h);
+  ctx.fillStyle = "white";
+  console.log(`Draw koch fractal at level ${level}`);
+  // ...
+}
+
+slider.addEventListener("change", (e) => {
+  drawKochFractal(e.target.value);
+});
+
+drawKochFractal(1);
